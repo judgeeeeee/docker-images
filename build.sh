60f0922d68139736a46799f4576c6be10da278a0
codeflitting <codeflitting@gmail.com>
2020-02-06 10:30:31 +0000

    finish

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-# Annaconda address
-# Anaconda2: https://repo.continuum.io/archive/Anaconda2-5.2.0-Linux-x86_64.sh
-# Anaconda3: https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
 
 function valid () {
     if [ $? -eq 0 ]; then
@@ -13,12 +10,13 @@ function valid () {
 
 
 #for package in anaconda caffe;do
-for package in anaconda tensorflow jupyter pytorch mxnet caffe scikit-learn;do
+#for package in anaconda tensorflow jupyter pytorch mxnet caffe scikit-learn;do
+for package in miniconda tensorflow keras pytorch mxnet caffe scikit-learn jupyterlab;do
     for version in  `ls $package`; do
     	echo "Start build -------> $package/$version"
 	docker build -t codeflitting/$package:$version $package/$version/ 
 	valid $package:$version
         docker push codeflitting/$package:$version
-	docker tag codeflitting/$package:$version cargo-infra.caicloud.xyz/devops_release/$package:$version
+	docker tag codeflitting/$package:$version cargo.caicloud.xyz/devops_release/$package:$version
     done
 done
